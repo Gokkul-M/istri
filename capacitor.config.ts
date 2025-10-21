0aea3786b80a37417adf60ad6cdcd8c929025dee
GokkulGokkul <21909115-GokkulGokkul@users.noreply.replit.com>
2025-10-21 06:04:25 +0000

    Restored to '3dbd9339be30c00c00b006408bc9a5f32aa962ca'
    Replit-Restored-To: 3dbd9339be30c00c00b006408bc9a5f32aa962ca

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.myaptpartner.laundry',
-  appName: 'My Apt Partner',
+  appId: 'com.istri.laundry',
+  appName: 'ShineCycle',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
